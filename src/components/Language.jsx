44a1ec61c7a4ac60a852256e976d7b1000722c87
madhavjari <213913746+madhavjari@users.noreply.github.com>
2025-12-02 15:44:06 +0530

    Created arrays for language and skills and rendered

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -2,47 +2,66 @@ import { useState } from "react";
 
 export default function Language() {
   const [showForm, setShowForm] = useState(false);
+  const [languages, setLanguages] = useState([]);
+  const [nextId, setNextId] = useState(1);
+
+  function LanguageList() {
+    return (
+      <>
+        {languages.map((language) => {
+          return <li key={language.id}>{language.name}</li>;
+        })}
+      </>
+    );
+  }
+
+  const handleLanguageChange = (language) => {
+    setLanguages((prevLang) => [...prevLang, { id: nextId, name: language }]);
+    setNextId(nextId + 1);
+    setShowForm(false);
+  };
+
   const showFormChange = () => {
     setShowForm(true);
   };
 
-  function LanguageForm() {
-    const [language, setLanguage] = useState("");
-    const handleLanguageChange = (event) => {
-      setLanguage(event.target.value);
-    };
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log(language);
-      resetForm();
-      setShowForm(false);
-    };
-
-    const resetForm = () => {
-      setLanguage("");
-    };
-    return (
-      <>
-        <form action="submit">
-          <input
-            type="text"
-            id="language"
-            placeholder="Add a language"
-            value={language}
-            onChange={handleLanguageChange}
-          />
-          <button type="submit" onClick={handleSubmit}>
-            Add
-          </button>
-        </form>
-      </>
-    );
-  }
   return (
     <>
       <h2>Language</h2>
       <button onClick={showFormChange}>Add a Language</button>
-      {showForm ? <LanguageForm /> : null}
+      {languages.length !== 0 ? <LanguageList /> : null}
+      {showForm ? <LanguageForm onAddLanguage={handleLanguageChange} /> : null}
+    </>
+  );
+}
+
+function LanguageForm({ onAddLanguage }) {
+  const [language, setLanguage] = useState("");
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (language.trim() !== "") {
+      onAddLanguage(language);
+      setLanguage("");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="language"
+          placeholder="Add a language"
+          value={language}
+          onChange={handleLanguageChange}
+        />
+        <button type="submit">Add</button>
+      </form>
     </>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,28 @@
 import { useState } from "react";
 
+function SkillForm({ onSkillChange }) {
+  const [skill, setSkill] = useState("");
+  const handleSkillChange = (event) => {
+    setSkill(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSkillChange(skill);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        id="skill"
+        placeholder="Add a skill"
+        value={skill}
+        onChange={handleSkillChange}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
 export default function Skills() {
   const [showFormTech, setShowFormTech] = useState(false);
   const [showFormSoft, setShowFormSoft] = useState(false);
@@ -10,80 +33,68 @@ export default function Skills() {
   const changeShowFormSoft = () => {
     setShowFormSoft(true);
   };
-
-  function SkillFormTech() {
-    const [skill, setSkill] = useState("");
-    const handleSkillChange = (event) => {
-      setSkill(event.target.value);
-    };
-
-    const resetForm = () => {
-      setSkill("");
-    };
-
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log(skill);
-      setShowFormTech(false);
-      resetForm();
-    };
+  function TechSkillList() {
     return (
-      <form>
-        <input
-          type="text"
-          id="skill"
-          placeholder="Add a skill"
-          value={skill}
-          onChange={handleSkillChange}
-        />
-        <button type="submit" onClick={handleSubmit}>
-          Add
-        </button>
-      </form>
+      <>
+        {techSkills.map((skill) => {
+          return <li key={skill.id}>{skill.name}</li>;
+        })}
+      </>
     );
   }
 
-  function SkillFormSoft() {
-    const [skill, setSkill] = useState("");
-    const handleSkillChange = (event) => {
-      setSkill(event.target.value);
-    };
-
-    const resetForm = () => {
-      setSkill("");
-    };
-
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log(skill);
-      setShowFormSoft(false);
-      resetForm();
-    };
+  function TechSkillList() {
     return (
-      <form>
-        <input
-          type="text"
-          id="skill"
-          placeholder="Add a skill"
-          value={skill}
-          onChange={handleSkillChange}
-        />
-        <button type="submit" onClick={handleSubmit}>
-          Add
-        </button>
-      </form>
+      <>
+        {techSkills.map((skill) => {
+          return <li key={skill.id}>{skill.name}</li>;
+        })}
+      </>
     );
   }
+  function SoftSkillList() {
+    return (
+      <>
+        {softSkills.map((skill) => {
+          return <li key={skill.id}>{skill.name}</li>;
+        })}
+      </>
+    );
+  }
+  const [techSkills, setTechSkills] = useState([]);
+  const [techNextId, setTechNextId] = useState(1);
+  const [softSkills, setSoftSkills] = useState([]);
+  const [softNextId, setSoftNextId] = useState(1);
+
+  const addTechSkill = (skill) => {
+    setTechSkills((prevSkill) => [
+      ...prevSkill,
+      { id: techNextId, name: skill },
+    ]);
+    setTechNextId(techNextId + 1);
+    setShowFormTech(false);
+  };
+
+  const addSoftSkill = (skill) => {
+    setSoftSkills((prevSkill) => [
+      ...prevSkill,
+      { id: softNextId, name: skill },
+    ]);
+    setSoftNextId(softNextId + 1);
+    setShowFormSoft(false);
+  };
 
   return (
     <>
       <h2>Skills</h2>
       <h3>Technical Skills:</h3>
       <button onClick={changeShowFormTech}>Add Skill</button>
-      {showFormTech ? <SkillFormTech /> : null}
+      {techSkills.length !== 0 ? <TechSkillList /> : null}
+      {showFormTech ? <SkillForm onSkillChange={addTechSkill} /> : null}
       <h3>Soft Skills</h3>
       <button onClick={changeShowFormSoft}>Add Skill</button>
-      {showFormSoft ? <SkillFormSoft /> : null}
+      {softSkills.length !== 0 ? <SoftSkillList /> : null}
+      {showFormSoft ? <SkillForm onSkillChange={addSoftSkill} /> : null}
     </>
   );
 }
